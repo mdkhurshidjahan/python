7d5a9ac0d0d319deb3ea4d35a04bf2099938dbf2
Md.Khurshid jahan <khurshid.jahan@northsouth.edu>
2022-09-09 20:48:51 +0600

    2

---
+++ 123.py
@@ -0,0 +1,7 @@
+character_name = "Jack"
+character_age = "70"
+print("There once was a pirate named " + character_name + ",")
+print("Jack was " + character_age + "years old.")
+character_name = "Mike"
+print("He really liked being " + character_name + ",")
+print("but he didn't like being " + character_age + ".")
